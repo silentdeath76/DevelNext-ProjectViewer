4bc97b2069f2866a8c30ee1c839b42004d0ec220
Max <smeex@mail.ru>
2022-07-08 01:43:43 +0300

    adds  dependencys
    Add new depenedencys
    Change about form

--- src/app/forms/About.php
+++ src/app/forms/About.php
@@ -12,19 +12,24 @@ class About extends AbstractForm
      */
     function doVboxConstruct(UXEvent $e = null)
     {    
+        $style = '-fx-font-size: 16px';
+        
         $e->sender->add($container = new UXHBox([new UXLabel("Сайт: "), $link = new UXHyperlink("GitHub")]));
-        $container->style = '-fx-font-size: 16px';
+        $container->style = $style;
         $container->spacing = 5;
         $link->on("action", function () {
             open('https://github.com/silentdeath76/DevelNext-ProjectViewer');
         });
         
         $e->sender->add($container = new UXHBox([new UXLabel("Автор: "), $link = new UXHyperlink("Vk")]));
-        $container->style = '-fx-font-size: 16px';
+        $container->style = $style;
         $container->spacing = 5;
         $link->on("action", function () {
             open('https://vk.com/silentrs');
         });
+        
+        $e->sender->add($container = new UXHBox([new UXLabel("Версия: "), new UXLabel("1.1.2")]));
+        $container->style = $style;
     }
 
 }
